e9634063d29d876d90bbefbbba09464a08c99b17
Parikshit <priyapawar32415@gmail.com>
2024-07-19 16:36:11 +0530

    Demo1 class created in BaseClass by Arya QA

---
+++ src/main/java/BaseLayer/Demo1.java
@@ -0,0 +1,13 @@
+package BaseLayer;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Demo1 class is created by Arya TESTER ");
+
+	}
+
+}
